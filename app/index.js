ae05ee3c25ea3832ce458e0adec3d0c8ee160c65
Laura Gutierrez <lauragsanahuja@gmail.com>
2023-10-12 07:05:56 +0200

    Develop characters from Game of Thrones

--- app/index.js
+++ app/index.js
@@ -4,11 +4,44 @@ import Knight from "./Knight/Knight.js";
 import Squier from "./Squier/Squier.js";
 import Adviser from "./Adviser/Adviser.js";
 
-const king = new King("Joffrey Baratheon");
-const knight = new Knight("Jamie Lannister");
-const warrior = new Knight("Daenerys Targaryen");
-const adviser = new Adviser("Tyrion Lannister", "Daenerys Targaryen");
-const squier = new Squier("Bronn", "Jaime Lannister");
+const king = new King(
+  "Joffrey Baratheon",
+  "Baratheon and Lannister",
+  13,
+  true,
+  "Game of Thrones",
+);
+const knight = new Knight(
+  "Jamie Lannister",
+  "House of Lannister",
+  41,
+  true,
+  "Game of Thrones",
+);
+const warrior = new Knight(
+  "Daenerys Targaryen",
+  "House of Targaryen",
+  13,
+  true,
+  "Game of Thrones",
+);
+const adviser = new Adviser(
+  "Tyrion Lannister",
+  "House of Lannister",
+  37,
+  true,
+  "Game of Thrones",
+  "Daenerys Targaryen",
+);
+
+const squier = new Squier(
+  "Bronn",
+  "Blackwaters",
+  32,
+  true,
+  "Game of Thrones",
+  "Jaime Lannister",
+);
 
 const charactersArray = [king, knight, warrior, adviser, squier];
 
